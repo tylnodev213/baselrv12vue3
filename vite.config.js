0fe62391b9a9e9f367b605222196e4c368faa068
tylnodev <fboyquangninh@gmail.om>
2026-03-09 03:08:39 +0700

    refs add manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    laravel({
+      input: ['resources/js/main.js'],
+      refresh: true,
+    }),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './resources/js'),
